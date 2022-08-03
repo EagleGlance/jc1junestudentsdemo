69e6be9e8efa81343e03599599f5ce315ac4b5be
Viachaslau Kalevich <cto@noirix.com>
2022-08-03 21:01:07 +0300

    Stream API

--- src/com/noirix/streamapi/StreamTest.java
+++ src/com/noirix/streamapi/StreamTest.java
@@ -1,12 +1,17 @@
 package com.noirix.streamapi;
 
+import com.noirix.domain.Animal;
 import com.noirix.domain.Cat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -17,7 +22,16 @@ public class StreamTest {
         cats.add(new Cat());
         cats.add(new Cat());
         cats.add(new Cat());
-        cats.add(new Cat());
+
+        Cat customCat = new Cat();
+        customCat.setColor("black, pink, green");
+        customCat.setWeight(5.7);
+        cats.add(customCat);
+
+        Cat cat1 = new Cat();
+        cat1.setColor("black, white, red");
+        cat1.setWeight(55);
+        cats.add(cat1);
 
         Stream<Cat> stream = cats.stream();
 
@@ -27,23 +41,55 @@ public class StreamTest {
         Stream<String> streamFromArrays = Arrays.stream(array);
 
         Predicate<Cat> ageAndNameFilter = cat -> cat.getAge() > 1 && cat.getName().contains("a");
-        Predicate<Cat> weightAndColorFilter = cat -> cat.getWeight() > 3 && cat.getColor().contains("o");
+        Predicate<Cat> weightAndColorFilter = cat -> cat.getWeight() >= 0  && cat.getColor() != null && cat.getColor().contains("a");
         Function<Cat, Cat> catUpdater = cat -> {
             cat.setAge(cat.getAge() + 1);
             cat.setColor("white");
             return cat;
         };
 
-        stream
-                .skip(2)
+        //Optional<Cat> first =
+        //long count =
+        //Set<Cat> collect =
+        //String collect =
+        Optional<Double> weightSum = stream
+                .skip(0)
 //                .filter(ageAndNameFilter)
-//                .filter(weightAndColorFilter)
-                .map(catUpdater)
-                .limit(3)
+                .filter(weightAndColorFilter)
+//                .map(catUpdater)
+                //.map(Cat::toString)
+                .map(Cat::getWeight)
+                //.peek(cat -> System.out.println("Cat name is: " + cat.getName()))
+                //.mapToLong(Animal::getId)
+                //.flatMap(cat -> Arrays.stream(cat.getColor().split(", ")))
+                //.limit(3)
                 .distinct()
+                //.sorted(Comparator.reverseOrder())
                 //.forEach(cat -> System.out.println(cat));
-                .forEach(System.out::println);
+                //.findFirst()
+                //.findAny()
+                //.collect(Collectors.toList())
+                //.collect(Collectors.toSet())
+                //.collect(Collectors.joining(",\n"))
+                //.collect(Collectors.)
+                //.min(Comparator.naturalOrder())
+                .reduce(Double::sum);
+        //.count()
+//.forEach(System.out::println)
 
+        //System.out.println(collect);
+
+        //System.out.println(count);
+
+        //first.ifPresent(System.out::println);
+        weightSum.ifPresent(System.out::println);
+//        if (first.isPresent()) {
+//            System.out.println(first.get());
+//        }
+
+//        for (Cat cat : collect) {
+//            System.out.println(cat);
+//        }
 
     }
 }
